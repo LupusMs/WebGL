9fb509e2e720fc1f7b26f6852652b3f3e9df46ef
Mykhailo Svyrydovych <mykhailo@froglogic.com>
2021-01-14 19:50:32 +0100

    Implement Assignment 4

--- Assignment4/Assignment4.js
+++ Assignment4/Assignment4.js
@@ -13,6 +13,8 @@ const aspect = canvas.width / canvas.height;
 // (compare slide 71 of chapter 4)
 const near = 0.1;
 const far = 100;
+const nearH = 2 * near * Math.sin((fov / 2) * Math.PI / 180);
+const nearW = aspect * nearH;
 const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
 camera.position.set(8,18,8);
 camera.lookAt(scene.position);
@@ -53,7 +55,7 @@ for(let k=1; k<=nBalls; ++k) {
 
 // * Implement picking and dragging dragging functionality
 
-let pickData = {};   // store data to be used across different 'pickOrDragBall' calls
+const pickData = [];   // store data to be used across different 'pickOrDragBall' calls
 
 canvas.addEventListener('mousedown', event => {
   // calculate viewport pixel position:
@@ -86,7 +88,49 @@ function pickOrDragBall(xvp, yvp, ball, idx, isMoveEvent) {
   console.assert(xvp>=0 && xvp<=canvas.width, 'xvp='+xvp);
   console.assert(yvp>=0 && yvp<=canvas.height, 'yvp='+yvp);
 
+  function fromViewportToCameraSpace(xvp, yvp, zClickCameraSpace) {
+    const xNDC = xvp * 2 / canvas.width - 1;
+    const yNDC = - yvp * 2 / canvas.height + 1;
+
+    const xClickCameraSpace = -(xNDC * nearW * zClickCameraSpace) / (2 * near);
+    const yClickCameraSpace = -(yNDC * nearH * zClickCameraSpace) / (2 * near);
+    
+    return new THREE.Vector3(xClickCameraSpace, yClickCameraSpace, zClickCameraSpace);
+  }
+  function fromCameraSpacetoNDC(vector) {
+    const xNDC = (-2 * near * vector.x) / (nearW * vector.z);
+    const yNDC = (-2 * near * vector.y) / (nearH * vector.z);
+    const zNDC = (far + near) / (far - near) + (2 * near * far) / ((far - near) * vector.z);
+    return new THREE.Vector3(xNDC, yNDC, zNDC);
+  }
+  function printData(distance,ballCoordinates, ballCameraSpace) {
+    console.log('Distance: ' + distance);
+    console.log('Ball center(world): ' + JSON.stringify(ballCoordinates));
+    console.log('Ball center(camera): ' + JSON.stringify(ballCameraSpace));
+    const ndcVector = fromCameraSpacetoNDC(ballCameraSpace);
+    console.log('Ball center(NDC): ' + JSON.stringify(ndcVector));
+  }
+
   // insert your code here:
+  const ballCameraSpace = ball.position.clone().applyMatrix4(camera.matrixWorldInverse);
+  if (isMoveEvent) {
+    const clickCameraSpace = fromViewportToCameraSpace(xvp, yvp, ballCameraSpace.z);
+    if (ball == pickData[0]) {
+      const clickWorld = clickCameraSpace.clone().applyMatrix4(camera.matrixWorld);
+      ball.position.x = clickWorld.x;
+      ball.position.y = clickWorld.y;
+      ball.position.z = clickWorld.z;
+    }
+  } else {
+    const pNear = fromViewportToCameraSpace(xvp, yvp, -near);
+    const n = pNear.clone().normalize();
+    const dist = ballCameraSpace.clone().add(n.clone().multiplyScalar(-n.clone().dot(ballCameraSpace))).length();
+    if (dist <= ball.userData.radius) {
+      pickData.push(ball);
+      ball.material.emissive = { r: ball.material.color.r * 3 / 4, g: ball.material.color.g * 3 / 4, b: ball.material.color.b * 3 / 4 };
+      printData(dist, ball.position, ballCameraSpace);
+    }
+  }
 
 }
 
@@ -94,7 +138,8 @@ function pickOrDragBall(xvp, yvp, ball, idx, isMoveEvent) {
 
 canvas.addEventListener('mouseup', event => {
   // insert your code here:
-
+  pickData.length = 0;
+  balls.forEach((b, idx) => { b.material.emissive = { r: 0, g: 0, b: 0 }; });
 });
 
 
